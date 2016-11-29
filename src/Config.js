468b12964e2377afdc9656f35e69588d80449553
Andrew Tran <andrewtran97@gmail.com>
2016-11-29 14:14:16 -0800

    Added Firebase Config

---
+++ src/Config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBSpbOlpyCbAFC5NeObciuYLhottdjy_Hs",
+    authDomain: "mamaslittlebakery-16032.firebaseapp.com",
+    databaseURL: "https://mamaslittlebakery-16032.firebaseio.com",
+    storageBucket: "mamaslittlebakery-16032.appspot.com",
+    messagingSenderId: "1090556586833"
+  };
+
+  export default FirebaseConfig;
