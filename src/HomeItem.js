de4f4edecedc6c6173ac431912c1e6cda849f55c
Jenna Han <jenna3@uw.edu>
2016-12-06 11:32:45 -0800

    header, home page styling changed

--- src/HomeItem.js
+++ src/HomeItem.js
@@ -4,17 +4,12 @@ import { Link } from 'react-router';
 // Returns a picture, contact information, and a bio
 var HomeItem = React.createClass({
     render() {
+    	//<img src={this.props.img} alt='background'/>
 		return(
 			<div className='home-item'>
 				<div className='card-panel'>
-					<div id='picture'>
-						<img src={this.props.img} alt='background'/>
-						<form action={this.props.link} className="homeButton">
-							<input type="submit" value={this.props.button}/>
-						</form>
-						<Link className='link' activeClassName='active' to='/bread'>Bread</Link>
-						<Link className='link' activeClassName='active' to='/store'>Store</Link>
-					</div>
+						<Link className='homelink' activeClassName='active' to='/blog'>Bread</Link>
+						<Link className='homelink' activeClassName='active' to='/store'>Store</Link>
 				</div>
 					
 			</div>
